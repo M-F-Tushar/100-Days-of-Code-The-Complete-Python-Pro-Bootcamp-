03d9477d59abd77c39083a2e4d6b91c8adae7ba6
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-21 00:48:18 +0600

    Add files via upload

---
+++ 📂 Day 1 - Beginner - Working with Variables in Python to Manage Data/1.printing practice.py
@@ -0,0 +1,6 @@
+# This programm will create a list of ingrediants
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knean the dough for 10 minute.")
+print("3. Add 3g of salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
