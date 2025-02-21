9a6752df9d96032b31fbd5ffa674a7bf68818611
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-21 22:34:53 +0600

    Delete 📂 Day 1 - Beginner - Working with Variables in Python to Manage Data/2. Debugging Practice.py

--- 📂 Day 1 - Beginner - Working with Variables in Python to Manage Data/2. Debugging Practice.py
+++
@@ -1,2 +0,0 @@
-
-print("Notes from Day 1\nThe printf statement is used to output strings\nStrings are strings of characters\nString Concatenation is done with the + sign\nNew lines can be created with a \ and the letter n")
