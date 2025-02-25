a2e589bbf9eee8e8a5473b51c09082d0e6ccbba1
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-25 17:41:59 +0600

    Add files via upload

---
+++ 📂 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/3.Mathematical Operation.py
@@ -0,0 +1,20 @@
+print("My age: " + str(123))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)
+print(2 ** 3)
+
+#PEMDAS(Parnthesis, Exponents, Multiplication, Division, Addition, Subtraction)
+
+# ()
+# **
+# * or /
+# + or -
+
+# What is the output of the code (3 * 3 + 3 / 3 - 3)
+print(3 * 3 + 3 / 3 - 3)
+
+# Change the code so it's output is 3
+print(3 * (3 + 3) / 3 - 3)
