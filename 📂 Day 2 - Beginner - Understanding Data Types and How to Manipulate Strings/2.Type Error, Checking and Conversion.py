8c4c7b9332fe6f6d10fc8cced2e2e166f031b4d2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-25 17:41:54 +0600

    Add files via upload

---
+++ 📂 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/2.Type Error, Checking and Conversion.py
@@ -0,0 +1,17 @@
+len("Hello")
+
+#Chencking Type
+print(type("abc"))
+print(type("123"))
+print(type("3.14"))
+print(type(True))
+
+#Type Convertion
+
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+
+print(type("Number of letters in your name: "))
+print(type(length_of_name))
+
+print("Numbers of letters in your name: " + str(length_of_name))
