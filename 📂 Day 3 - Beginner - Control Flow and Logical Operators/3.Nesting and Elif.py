382a8ec569770fca0bc3b995656796b22965a4c5
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-28 11:56:50 +0600

    Add files via upload

---
+++ 📂 Day 3 - Beginner - Control Flow and Logical Operators/3.Nesting and Elif.py
@@ -0,0 +1,14 @@
+print("Welcome to the rolllercoaster!")
+height = int(input("what is your height is cm?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age?"))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("please Pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry you have to grorw taller beore you can ride.")    
