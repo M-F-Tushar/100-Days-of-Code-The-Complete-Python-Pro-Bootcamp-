a1031473a71c30e37709ffed0ceba87b34e58a43
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-28 11:56:26 +0600

    Add files via upload

---
+++ 📂 Day 3 - Beginner - Control Flow and Logical Operators/4.Multiple Ifs.py
@@ -0,0 +1,24 @@
+print("Welcome to the rolllercoaster!")
+height = int(input("what is your height is cm?"))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age?"))
+    if age <= 12:
+        bill = 5
+        print("Child pay $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth Pay $7.")
+    else:
+        bill = 12
+        print("Adult pay $12.")
+
+    wants_photo = input("Do you want to have a photo take? Take Y for Yes and n for No ")
+    if wants_photo == "Y":
+        bill += 3 #Add $3 t their bill
+    
+    print(f"Your final bill is ${bill}")
+else:
+    print("Sorry you have to grorw taller beore you can ride.")    
