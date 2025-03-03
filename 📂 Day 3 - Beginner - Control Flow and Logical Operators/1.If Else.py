df7da1f95ac9367ff259f139b4b2d1e59da80194
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-03 21:38:31 +0600

    Update 1.If Else.py

--- 📂 Day 3 - Beginner - Control Flow and Logical Operators/1.If Else.py
+++ 📂 Day 3 - Beginner - Control Flow and Logical Operators/1.If Else.py
@@ -5,6 +5,5 @@ height = int(input("what is your height is cm?"))
 
 if height >= 120:
     print("You can ride the rollercoaster")
-    
 else:
     print("Sorry you have to grorw taller beore you can ride.")    
