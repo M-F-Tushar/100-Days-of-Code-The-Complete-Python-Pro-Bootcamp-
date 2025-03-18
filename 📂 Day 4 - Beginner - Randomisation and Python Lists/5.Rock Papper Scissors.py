ea463d36d5dc748a5fc6d22cb0be2ff3f0dba745
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-03-19 04:43:20 +0600

    Update 5.Rock Papper Scissors.py

--- 📂 Day 4 - Beginner - Randomisation and Python Lists/5.Rock Papper Scissors.py
+++ 📂 Day 4 - Beginner - Randomisation and Python Lists/5.Rock Papper Scissors.py
@@ -1,7 +1,7 @@
 #You are going to build a Rock, Paper, Scissors game. You will need to use what you have learnt about randomisation and Lists to achieve this.
 
 #Demo
-#https://appbrewery.github.io/python-day4-demo/
+# https://appbrewery.github.io/python-day4-demo/
 
 import random
 
